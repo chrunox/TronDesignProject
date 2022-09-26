3a3e4970ec7e883a7674cb44a5e44be8c7a99298
chrunox <77234246+chrunox@users.noreply.github.com>
2022-09-26 17:14:15 -0400

    Add files via upload

--- motor_testing_ML/TOF_sensor.ino
+++ motor_testing_ML/TOF_sensor.ino
@@ -29,3 +29,26 @@ void setID() {
     while (1);
   }
 }
+
+int read_sensor (int sensorID) {
+  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
+  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
+  switch (sensorID) {
+    case 1:
+      if (measure1.RangeMilliMeter < 200) {
+        return measure1.RangeMilliMeter;
+      } else {
+        //Serial.println("Left sensor out of range");
+        return -1;
+      }
+      break;
+    case 2:
+      if (measure2.RangeMilliMeter < 200) {
+        return measure2.RangeMilliMeter;
+      } else {
+        //Serial.println("Right sensor out of range");
+        return -1;
+      }
+      break;
+  }
+}
--- motor_testing_ML/motor_testing_ML.ino
+++ motor_testing_ML/motor_testing_ML.ino
@@ -31,92 +31,63 @@ VL53L0X_RangingMeasurementData_t measure1;
 VL53L0X_RangingMeasurementData_t measure2;
 
 void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+
+  //set pins for encoders
   pinMode(ENCODER1, INPUT);
   pinMode(ENCODER2, INPUT);
   pinMode(ENCODER3, INPUT);
   pinMode(ENCODER4, INPUT);
+
+  //set pins for TOF sensors
+  pinMode(SHT_LOX1, OUTPUT);
+  pinMode(SHT_LOX2, OUTPUT);
+
+  //reset and initialize TOF sensors
+  Serial.println(F("Shutdown pins inited..."));
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+  Serial.println(F("Both in reset mode...(pins are low)"));
+  Serial.println(F("Starting..."));
+  setID();
+
   attachInterrupt(digitalPinToInterrupt(ENCODER1), encoder1, RISING);
   attachInterrupt(digitalPinToInterrupt(ENCODER2), encoder2, RISING);
   attachInterrupt(digitalPinToInterrupt(ENCODER3), encoder3, RISING);
   attachInterrupt(digitalPinToInterrupt(ENCODER4), encoder4, RISING);
-  Serial.begin(9600);
 }
 
 void loop() {
   //  motor.setSpeed(128);  // Run forward at 50% speed.
   //  delay(2000);
 
-  //  float timeold;
-  //  float timecurr;
-  //  float pos1old;
-  //  float pos1curr;
-  //  float pos2old;
-  //  float pos2curr;
-  //  float timetemp;
+  //    float timeold;
+  //    float timecurr;
+  //    float pos1old;
+  //    float pos1curr;
+  //    float pos2old;
+  //    float pos2curr;
+  //    float timetemp;
   //
-  //  for (float i = 0; i < 255; i++) { //loop to measure the motor speed in pulses per sec
-  //    timeold = timecurr;
-  //    timecurr = millis();
-  //    pos1old = pos1curr;
-  //    pos1curr = encoder1_pos;
-  //    pos2old = pos2curr;
-  //    pos2curr = encoder3_pos;
+  //  int turn_time = 1000;
   //
-  //    motor1.setSpeed(i);        // Run bad motor forward at full speed.
-  //    motor2.setSpeed(i * 0.9);  // Run good motor forward at full speed. Tuned PWM by multiplying with 0.9 to try to match other motor's speed profile
-  //
-  //    float motor1_speed = (pos1curr - pos1old) * 10 / (timecurr - timeold);
-  //    float motor2_speed = (pos2curr - pos2old) * 10 / (timecurr - timeold);
-  //
-  //    Serial.println(String(motor1_speed) + " " + String(motor2_speed));
-  //    delay(50);
-  //  }
-  int turn_time = 1000;
-
-  Left(turn_time);
-  Forward(1);
-  Right(turn_time);
-  Forward(2);
-  Right(turn_time);
-  Forward(3);
-  Left(turn_time);
-  Forward(4);
-}
-
-
-
-
-
-
-
-void encoder1() {
-  if (digitalRead(ENCODER1) == HIGH) {
-    encoder1_pos++;
-  } else {
-    encoder1_pos--;
-  }
-}
-
-void encoder2() {
-  if (digitalRead(ENCODER2) == HIGH) {
-    encoder2_pos++;
-  } else {
-    encoder2_pos--;
-  }
-}
-
-void encoder3() {
-  if (digitalRead(ENCODER3) == HIGH) {
-    encoder3_pos++;
-  } else {
-    encoder3_pos--;
-  }
-}
-
-void encoder4() {
-  if (digitalRead(ENCODER4) == HIGH) {
-    encoder4_pos++;
-  } else {
-    encoder4_pos--;
-  }
+  //  Left(turn_time);
+  //  Forward(1);
+  //  Right(turn_time);
+  //  Forward(2);
+  //  Right(turn_time);
+  //  Forward(3);
+  //  Left(turn_time);
+  //  Forward(4);
+  char F1[] = "ACBDA";
+  Forward(F1);
+  Right(1000);
+  Forward(F1);
+  Left(1000);
+  
 }
